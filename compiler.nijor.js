31766d9350849e676e76bf8343dcd9f52331b713
DebAxom <debaxom@gmail.com>
2021-05-01 13:54:20 +0530

    Updated the repo

--- compiler.nijor.js
+++ compiler.nijor.js
@@ -1,12 +1,16 @@
 const rollup = require('rollup');
 const includePaths = require('rollup-plugin-includepaths');
-const terser = require('rollup-plugin-terser');
+const {terser} = require('rollup-plugin-terser');
 const fs = require('fs');
 const path = require('path');
 const sass = require('sass');
+const {nodeResolve} = require('@rollup/plugin-node-resolve');
+const cjs = require('@rollup/plugin-commonjs');
+const image = require('@rollup/plugin-image');
+const asyncFn = require('rollup-plugin-async');
 const NijorCompiler = require('@nijor/nijor-rollup-plugin');
 const srcPath = path.join(__dirname,'src');
-let includePathOptions = {
+const includePathOptions = {
     include: {
         'nijor':path.join(__dirname,'node_modules/@nijor/nijor/src/nijor.js'),
         'nijor/components':path.join(__dirname,'node_modules/@nijor/nijor/src/components.js'),
@@ -18,35 +22,44 @@ let includePathOptions = {
     },
     paths: [srcPath],
     external: [],
-    extensions: ['.js','.nijor']
+    extensions: ['.js','.nijor','.svg','.jpg','.png']
 };
+function Style(style){
+    let cssStyle = sass.renderSync({
+        data:style,
+        outputStyle:'compressed'
+    });
+    return cssStyle.css.toString();
+}
+const compilerOptions = {
+    styleSheet:path.join(__dirname,'app/static/style.css'),
+    Style
+}
 const inputOptions = {
     input:'src/App.js',
     plugins:[
         includePaths(includePathOptions),
-        NijorCompiler(__dirname),
-        terser.terser()
+        nodeResolve(),
+        cjs(),
+        NijorCompiler(compilerOptions),
+        asyncFn(),
+        image(),
+        terser()
     ]
 };
 const outputOptions = {
     file:'app/static/app.js',
-    format:'iife',
+    format:'es',
 };
 async function build() {
-  console.log(`Nijor: Compiling the files.`);
-  const bundle = await rollup.rollup(inputOptions);
-  const { output } = await bundle.generate(outputOptions);
-  for (const chunkOrAsset of output) {
-    if (chunkOrAsset.type === 'asset') {
-    } else {
-    }
-  }
-  await bundle.write(outputOptions);
-  await bundle.close();
-  console.log(`Nijor: Compiled all files successfully.`);
+    console.log(`Nijor: Compiling the files.`);
+    const bundle = await rollup.rollup(inputOptions);
+    await bundle.write(outputOptions);
+    await bundle.close();
+    console.log(`Nijor: Compiled all files successfully.`);
 }
 let globalStyles = fs.readFileSync('./src/styles/style.scss','utf-8');
-let cssStyle = sass.renderSync({
+const cssStyle = sass.renderSync({
     data:globalStyles,
     outputStyle:'compressed'
 });
